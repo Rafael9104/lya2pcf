7a0362af4d70ed32a476dcd72dcc37ce518f3662
Rafael9104 <98924969+Rafael9104@users.noreply.github.com>
2025-03-17 18:08:31 -0600

    Update run_with_shuffled.sh

--- run_with_shuffled.sh
+++ run_with_shuffled.sh
@@ -5,13 +5,13 @@ save_dir='/home/josue/cosmologia/three_point/3pla/outputs/dr14.1/'
 
 # exit 0  
 #echo 'Running ordered data set'
-#python 3pla.py --from-extracted --cuda --two-point --save-dir=$save_dir
+#python 2pla.py --from-extracted --cuda --two-point --save-dir=$save_dir
 
 for i in $(seq 21 1 2000)
 do
 echo "Running $i shuffled data set"
 python shuffler.py --number=${i}
 # mkdir ${save_dir}"/shuffle_ra_dec_"${i}"/"
-python 3pla.py --from-extracted --cuda --two-point --save-dir=${save_dir}"/shuffle_ra_dec_"${i}"/"
+python 2pla.py --from-extracted --cuda --two-point --save-dir=${save_dir}"/shuffle_ra_dec_"${i}"/"
 rm ${save_dir}"/shuffle_ra_dec_"${i}"/data.npy"
 done
