0ce8057c844d3800a37128cf4165eab7c0cb2a72
Rafael Gutiérrez-Balboa <98924969+Rafael9104@users.noreply.github.com>
2026-03-23 14:01:40 -0600

    Fixed error in line 26 of 2pla_multiple_data.py
    Fixed error in line 26 that was causing a loop.

--- 2pla_multiple_data.py
+++ 2pla_multiple_data.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     os.environ['CUDA_DEVICE'] = cuda_device
 
     # Writing log files, one per mpi process
-    if not os.path.exists(corr_dir):
-        os.makedirs(corr_dir)
+    #if not os.path.exists(corr_dir):
+    os.makedirs(corr_dir, exist_ok=True)
     log_filename = corr_dir + 'thread_' + str(mpi_rank) + '_of_' + str(mpi_size) + '.log'
     log_file = open(log_filename,"w+")
 
